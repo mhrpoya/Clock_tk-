6c0a5de3033384e902f8122ceefe4e99f1d4469e
mhrpoya <mehrpoyakhosravi49@gmail.com>
2021-12-15 21:42:32 +0330

    new2

--- Clock2.py
+++ Clock2.py
@@ -4,25 +4,32 @@ import time
 
 def times():
     current_time = time.strftime("%H:%M:%S")
-    clock.config(text=current_time, bg="#ffffff", fg="#252c41")
+    clock.config(text=current_time, bg="#101b33", fg="white")
     clock.after(200,times)
 
 
 win = tk.Tk()
 win.title("Clock")
-win.geometry("347x215+600+250")
+win.geometry("347x220+600+250")
 win.resizable(0,0)
-win.config(bg="#394465")
-clock = tk.Label(win,font=("times",30,"bold"),bg="white")
+
+
+canvas_width = 347
+canvas_height = 220
+can1 = tk.Canvas(win, width= canvas_width, height= canvas_height, bg="#13254e", relief="groove", bd=5)
+can1.create_text(274, 215, fill="#13264e")
+can1.pack()
+
+
+clock = tk.Label(can1,font="times 30 bold" ,bg="white")
 clock.grid(row=2,column=2,pady=50,padx=95)
 times()
 
-digi = tk.Label(win,text="Digital Clock", font="times 24 bold", bg="white", fg="#252c41")
-digi.grid(row=0,column=2)
+digi = tk.Label(can1,text="Digital Clock", font="times 24 bold", bg="#101b33", fg="white")
+digi.grid(row=1,column=2)
 
 
-nota= tk.Label(win, text="Hours | Minutes | Seconds", font="times 13 bold")
-
+nota= tk.Label(can1, text= "  Hours  |   Minutes  |  Seconds ", font="times 15 bold", bg="#101b33", fg="white")
 nota.grid(row=3,column=2)
 
 win.mainloop()
